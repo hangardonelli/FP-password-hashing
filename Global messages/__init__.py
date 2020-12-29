37a26057fff043af92b44dee4b657ec946ff64bf
hangardonelli <57008563+hangardonelli@users.noreply.github.com>
2020-12-28 23:18:45 -0300

    Update __init__.py

--- Global messages/__init__.py
+++ Global messages/__init__.py
@@ -9,9 +9,9 @@ import asyncio
 from houdini.data.moderator import Ban
 
 
-class Essentials(IPlugin):
-    author = "Solero"
-    description = "Essentials plugin"
+class GlobalMsgs(IPlugin):
+    author = "Lau"
+    description = "Global message plugin for Freeze Penguin"
     version = "1.0.0"
 
     def __init__(self, server):
@@ -20,13 +20,13 @@ class Essentials(IPlugin):
         self.items_by_name = None
 
     
-        await self.server.permissions.register('essentials.gm')
+        await self.server.permissions.register('gm')
         self.items_by_name = {item.name: item for item in self.server.items.values()}
 
    
 
     @commands.command('gm')
-    @permissions.has_or_moderator('essentials.gm')
+    @permissions.has_or_moderator('gm')
     async def global_msg(self, p, *message: str):
         message = ' '.join([i for i in message])
         await p.room.send_xt('cerror', message, 'Freeze Penguin')
